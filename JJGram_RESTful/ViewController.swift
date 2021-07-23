104e42b0c8d904f3bd5985cc83ab603032e67a1d
heogj123 <>
2021-07-23 12:00:56 +0900

    (*) RESTful API Image 파싱 테스트

--- JJGram_RESTful/ViewController.swift
+++ JJGram_RESTful/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 import Alamofire
+import SnapKit
+import SDWebImage
 
 class ViewController: UIViewController {
-
+  
+  let imageView = UIImageView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     self.view.backgroundColor = .white
+    
+    self.view.addSubview(imageView)
+    imageView.snp.makeConstraints { (make) in
+      make.width.height.equalTo(150)
+      make.center.equalTo(self.view)
+    }
+    
+    imageView.layer.borderWidth = 1.0
+    imageView.layer.borderColor = UIColor.cyan.cgColor
+    
+    let url = URL.init(string: "http://192.168.0.22:3000/image")
+    imageView.sd_setImage(with: url, completed: nil)
   }
   
   
